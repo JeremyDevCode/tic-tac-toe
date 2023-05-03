cd1ed85d97c7902740d8f0451bbb04537d237654
JeremyDevCode <jeremydevcode@gmail.com>
2023-05-03 01:37:35 -0500

    feat: add reset button

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -14,11 +14,11 @@ function Square({ children, index, updateBoard }: SquareProps) {
   return (
     <div
       onClick={handleClick}
-      className={`grid place-items-center w-[100px] h-[100px] border-[#E3E6E8] 
-      ${index < 3 && "border-b-2"} 
-      ${index > 5 && "border-t-2"} 
-      ${(index === 0 || index === 3 || index === 6) && "border-r-2"}
-      ${(index === 2 || index === 5 || index === 8) && "border-l-2"}`}
+      className={`grid place-items-center w-[100px] h-[100px] border-[#E3E6E8] ${
+        index < 3 ? "border-b-2" : null
+      } ${index > 5 ? "border-t-2" : null} ${
+        index === 0 || index === 3 || index === 6 ? "border-r-2" : null
+      } ${index === 2 || index === 5 || index === 8 ? "border-l-2" : null}`}
     >
       {children}
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,12 @@ export default function Home() {
   const [winner, setWinner] = useState<boolean | null>(null);
   const [turn, setTurn] = useState(TURNS.X);
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
   function updateBoard(index: number) {
     if (board[index] || winner) return;
 
@@ -38,6 +44,19 @@ export default function Home() {
           );
         })}
       </section>
+      <button
+        className={`text-[#0D0F12] py-2 px-5 rounded-lg font-semibold ${
+          board.every((square) => square !== null) ? "hover:bg-[#D3D6D8]" : ""
+        } ${
+          board.every((square) => square === null)
+            ? "bg-[#999]"
+            : "bg-[#E3E6E8]"
+        }`}
+        disabled={board.every((square) => (square === null ? true : false))}
+        onClick={resetGame}
+      >
+        Resetear
+      </button>
     </main>
   );
 }
